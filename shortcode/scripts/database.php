d59f036cf5fcbb5b25f7169792e359886511bafb
pneyrinck <paul@jammyapp.com>
2017-08-16 09:28:25 -0700

    added file to help with running a form outside of Wordpress for debugging

---
+++ shortcode/scripts/database.php
@@ -0,0 +1,7 @@
+<?php /**/ ?><?php
+// this file can assist with local debugging
+//$GLOBALS['ncf_server'] = "localhost";
+//$GLOBALS['ncf_user'] = "root";
+//$GLOBALS['ncf_password'] = "28hr37YS2";
+//$GLOBALS['ncf_database'] = "test1";
+?>
