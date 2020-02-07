a4c36ffee3b9f6f974dcd4ac0089f0ed0ce4372d
pneyrinck <paul@neyrinck.com>
2020-02-06 20:26:22 -0800

    improved eden test to pass JSON data

--- shortcode/scripts/test-eden-proxy.php
+++ shortcode/scripts/test-eden-proxy.php
@@ -5,7 +5,11 @@ curl -XPOST -H "Content-type: application/json" -d '{"accountId":"neyrincktest",
 
 
 // From URL to get webpage contents. 
-$url = "https://test.neyrinck.com/findUserByAccountId"; 
+$url = "https://test.neyrinck.com/findUserByAccountId";
+
+$data = array("accountId" => "neyrincktest", "accessKeyStr" => "FQKoGGpkMU9QTHpTcWRlODVsZXNOdUp1ZXc9PRwVAqUAFQIVApnzEETBWA55jdnuD6KGr2qW7IoAAA==");                                                                    
+$data_string = json_encode($data);
+
 $curl = curl_init();
 curl_setopt ($curl, CURLOPT_URL, "https://test.neyrinck.com/findUserByAccountId");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -17,9 +21,17 @@ curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($curl, CURLOPT_POSTREDIR, 3);
 
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);                                                                  
+curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',                                                                                
+    'Content-Length: ' . strlen($data_string))                                                                       
+);
+
+
 $result = curl_exec ($curl);
 
 echo "$result<br>";
+//echo $data_string;
 curl_close ($curl);
 
 ?>
