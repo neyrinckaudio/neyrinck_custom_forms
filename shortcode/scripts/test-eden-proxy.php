64125345a9ee2d96e68516d157e15ebb671ba235
pneyrinck <paul@neyrinck.com>
2020-02-06 20:34:36 -0800

    factored doEden function

--- shortcode/scripts/test-eden-proxy.php
+++ shortcode/scripts/test-eden-proxy.php
@@ -1,37 +1,34 @@
 <?php
-/*
-curl -XPOST -H "Content-type: application/json" -d '{"accountId":"neyrincktest","accessKeyStr":"FQKoGGpkMU9QTHpTcWRlODVsZXNOdUp1ZXc9PRwVAqUAFQIVApnzEETBWA55jdnuD6KGr2qW7IoAAA=="}' 'https://test.neyrinck.com/findUserByAccountId/'
-*/
 
+function doEden($functionName, $data)
+{
+    $data_string = json_encode($data);
+    
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://test.neyrinck.com/$functionName");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_VERBOSE, true);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($curl, CURLOPT_POSTREDIR, 3);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);                                                                  
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                                          
+        'Content-Type: application/json',                                                                                
+        'Content-Length: ' . strlen($data_string))                                                                       
+    );
+    $result = curl_exec ($curl);
+    curl_close ($curl);
+    return $result;
+}
 
 // From URL to get webpage contents. 
-$url = "https://test.neyrinck.com/findUserByAccountId";
-
 $data = array("accountId" => "neyrincktest", "accessKeyStr" => "FQKoGGpkMU9QTHpTcWRlODVsZXNOdUp1ZXc9PRwVAqUAFQIVApnzEETBWA55jdnuD6KGr2qW7IoAAA==");                                                                    
-$data_string = json_encode($data);
-
-$curl = curl_init();
-curl_setopt ($curl, CURLOPT_URL, "https://test.neyrinck.com/findUserByAccountId");
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-//curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_VERBOSE, true);
-
-curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($curl, CURLOPT_POSTREDIR, 3);
-
-curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);                                                                  
-curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                                          
-    'Content-Type: application/json',                                                                                
-    'Content-Length: ' . strlen($data_string))                                                                       
-);
-
-
-$result = curl_exec ($curl);
-
+$result = doEden('findUserByAccountId', $data);
 echo "$result<br>";
-//echo $data_string;
-curl_close ($curl);
+
+$data = array("accountId" => "neyrinck", "accessKeyStr" => "FQKoGGpkMU9QTHpTcWRlODVsZXNOdUp1ZXc9PRwVAqUAFQIVApnzEETBWA55jdnuD6KGr2qW7IoAAA==");                                                                    
+$result = doEden('findUserByAccountId', $data);
+echo "$result<br>";
 
 ?>
