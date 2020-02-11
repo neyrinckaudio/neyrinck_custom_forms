3c2822c7a660c82aa928144a8fb05b0eaed55e38
pneyrinck <paul@neyrinck.com>
2020-02-11 10:57:02 -0800

    improved text

--- shortcode/scripts/vcpTrialSubmitAccountForm.php
+++ shortcode/scripts/vcpTrialSubmitAccountForm.php
@@ -12,7 +12,7 @@
         });
     });
 </script>
-
+<p>V-Control Pro operates when unlicensed, but features are limited. You can register for a trial license to enable all features for a limited amount of time. You must have an iLok user account to get a trial license. www.ilok.com.</p>
 	<form name="activate_first" id="activate_first" method="POST" action="">
     <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
     <input type="hidden" name="action" value="validate_captcha">
