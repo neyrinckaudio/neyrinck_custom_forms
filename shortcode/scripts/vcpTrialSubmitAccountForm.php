ef50132c3d5485da61ef9cba2331ce7155bcba48
pneyrinck <paul@neyrinck.com>
2020-02-11 09:32:35 -0800

    new files for vcp trial form

---
+++ shortcode/scripts/vcpTrialSubmitAccountForm.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<script src="https://www.google.com/recaptcha/api.js?render=6LfduNcUAAAAAMCW0-tF_IDCz2gew_xTS1wYW2Mh"></script>
+<script>
+    grecaptcha.ready(function() {
+    // do request for recaptcha token
+    // response is promise with passed token
+        grecaptcha.execute('6LfduNcUAAAAAMCW0-tF_IDCz2gew_xTS1wYW2Mh', {action:'vcptrial'})
+                  .then(function(token) {
+            // add token value to form
+            document.getElementById('g-recaptcha-response').value = token;
+        });
+    });
+</script>
+
+	<form name="activate_first" id="activate_first" method="POST" action="">
+    <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
+    <input type="hidden" name="action" value="validate_captcha">
+		<p>Please enter your ilok User Id.</p>
+		<p><label>iLok User ID:  &nbsp; &nbsp;</label><input name="item_account_id" id="item_account_id" value="" type="text"></p>
+		<p><input class="et_pb_promo_button et_pb_button" type="submit" name="submitAccountId" id="first_submit" value="Submit iLok Account Id"></p>
+	</form>
+<?php
+?>
---
+++ shortcode/scripts/vcptrial.php
@@ -0,0 +1,124 @@
+<?php
+include_once('dbFunctions.php');
+include_once('ilokFunctions.php');
+$vcpTrialGUID = "2A666090-6653-11E6-80BB-005056A204F3";
+//Use this site key in the HTML code your site serves to users
+//6LfduNcUAAAAAMCW0-tF_IDCz2gew_xTS1wYW2Mh
+
+//Use this secret key for communication between your site and reCAPTCHA
+//6LfduNcUAAAAAMcvZOqItm0PLqH-x3yjMwEtdqXy server ca[tcha 3 key]
+function sendEmail($product_name, $first_name, $last_name, $email1, $activation_code, $ilok_id, $company, $licenseRef)
+{
+    $subject = "Neyrinck $product_name Activation";
+    $to = "store@neyrinck.com";
+   
+    
+    $headers = "From: Neyrinck <store@neyrinck.com>" . "\r\n";
+   // $headers .= 'Bcc: berniceling@neyrinck.com' . "\r\n";
+    $headers .= "Reply-To: $first_name $last_name<$email1>" . "\r\n";
+
+    $mail_cont = "Name: $first_name $last_name\r\nActivation Code: $activation_code\r\nLicense Reference: $licenseRef\r\niLok ID: $ilok_id\r\nCompany: $company\r\nEmail: $email1\r\n";
+
+    $mail_cont .= "\n\nDear $first_name,\n\nWe have processed activation code $activation_code and delivered a $product_name iLok license to User ID $ilok_id.";
+    
+    $mail_cont .= " The latest version of $product_name can be downloaded here:\n\n";
+    $mail_cont .= "http://www.neyrinck.com/downloads\n\n";
+    $mail_cont .= "Thank you for purchasing $product_name.\n\nSincerely,\nPaul Neyrinck";
+
+    // mail to store@neyrinck.com
+    mail($to, $subject, $mail_cont, $headers);
+
+    // mail to customer
+    $to = "$first_name $last_name<$email1>";
+    $headers = "From: Neyrinck <store@neyrinck.com>" . "\r\n";
+    //$headers .= 'Bcc: bernice@rejamm.com' . "\r\n";
+    $headers .= "Reply-To: Neyrinck<store@neyrinck.com>" . "\r\n";
+    mail($to, $subject, $mail_cont, $headers);
+}
+
+if (isset($_POST['submitAccountId']) && !empty($_POST['item_account_id']))
+{
+    if (isset($_POST['g-recaptcha-response'])) {
+        $captcha = $_POST['g-recaptcha-response'];
+    } else {
+        $captcha = false;
+    }
+    
+    if (!$captcha) {
+        echo "Google captcha3 not detected.<br>";
+        die();
+    } else {
+        $secret   = '6LfduNcUAAAAAMcvZOqItm0PLqH-x3yjMwEtdqXy';
+        $response = file_get_contents(
+            "https://www.google.com/recaptcha/api/siteverify?secret=" . $secret . "&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']
+        );
+        // use json_decode to extract json response
+        $response = json_decode($response);
+    
+        if ($response->success === false) {
+            echo "Google captcha3 error.<br>";
+            die();
+        }
+    }
+    $score = $response->score;
+    //... The Captcha is valid you can continue with the rest of your code
+    //... Add code to filter access using $response . score
+    if ($response->success==true && $response->score <= 0.5) {
+        
+        echo "Google captcha3 believes this is spam. score = $score<br>";
+        die();
+    }
+
+    try 
+    {
+        $ilok_user_id = $_POST['item_account_id'];
+        $ilokIdTest = findUserByAccountId($ilok_user_id);
+        if ($ilokIdTest != $ilok_user_id)
+        {
+            throw new Exception("iLok User ID is not valid");
+        }
+        $licenses = findUserLicenseBySKU($vcpTrialGUID, $ilok_user_id);
+        if (count($licenses) > 0)
+        {
+            foreach ($licenses as $iter69)
+            {
+            //echo json_encode($iter69);
+            //echo "<br><br>";
+            }
+            $depositedDright = $licenses[0];
+            $licenseRef = $depositedDright['drightGuid'];
+            echo "The iLok system indicates your iLok account already has a trial license.<br>";
+            echo "License Reference: $licenseRef<br>";
+        }
+        else
+        {
+            $orderId = date('Y-m-d H:i:s') . "VCPTRIAL";
+            $drightGuidArray = depositFullLicense($vcpTrialGUID, $ilok_user_id, $orderId);
+            // $drightGuidArray is null if it failed
+            if (drightGuidArray == null) {
+                throw new Exception("License depost failed.");
+            }
+//            echo json_encode($drightGuidArray);
+            $depositedDrights = $drightGuidArray['depositedDrights'];
+            $depositedDright = $depositedDrights[0];
+            $licenseRef = $depositedDright['drightGuid'];
+
+        echo "A trial license has been deposited to account $ilok_user_id. When you are ready to try V-Control Pro, launch iLok License Manager on your computer and activate the license.<br>";
+        echo "License Reference: $licenseRef<br>";
+        //include_once('vcpTrialSubmitRegistrationForm.php');
+        }
+    }
+    catch  (Exception $e)
+    {
+        $errorMsg = $e->getMessage();
+        echo "$errorMsg";
+        include_once('vcpTrialSubmitAccountForm.php');
+        
+    }
+}
+
+else
+{
+    include_once('vcpTrialSubmitAccountForm.php');
+}
+?>
