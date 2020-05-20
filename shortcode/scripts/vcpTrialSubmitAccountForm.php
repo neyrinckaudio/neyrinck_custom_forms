1cda0daebd15a8a383694acd390169e589425e97
pneyrinck <paul@neyrinck.com>
2020-05-20 13:36:35 -0700

    removed autocorrection for email address

--- shortcode/scripts/vcpTrialSubmitAccountForm.php
+++ shortcode/scripts/vcpTrialSubmitAccountForm.php
@@ -17,7 +17,7 @@
     <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
     <input type="hidden" name="action" value="validate_captcha">
 		<p>Please enter your ilok User Id.</p>
-		<p><label>iLok User ID:  &nbsp; &nbsp;</label><input name="item_account_id" id="item_account_id" value="" type="text"></p>
+		<p><label>iLok User ID:  &nbsp; &nbsp;</label><input name="item_account_id" id="item_account_id" value="" type="text" autocorrect="off" autocapitalize="none"></p>
 		<p><input class="et_pb_promo_button et_pb_button" type="submit" name="submitAccountId" id="first_submit" value="Submit iLok Account Id"></p>
 	</form>
 <?php
