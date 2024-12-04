f1f8c9e0bf75c5152d31ea108a5c5844efb88e8f
pneyrinck <paul@neyrinck.com>
2024-12-04 15:18:17 -0800

    improved license deposit followup

--- shortcode/scripts/activationSuccess.php
+++ shortcode/scripts/activationSuccess.php
@@ -1,6 +1,7 @@
 <?php
 ?>
-	<p style="padding-top: 50px;">The license has been deposited to your iLok.com account. Please use the iLok License Manager to transfer the license to your iLok USB key/machine.</p>
+	<p style="padding-top: 50px;">The license has been deposited to your iLok.com account. Please use the iLok License Manager to activate the license to your iLok USB key/machine.</p>
+	<p>You can download an installer <a href='https://neyrinck.com/downloads/'>HERE</a>. If you already installed a trial version, you do not need to re-install.</p>
 	<p>Please contact <a href='mailto:support@neyrinck.com'>support@neyrinck.com</a> if you need any help.</p>
 <?php
 ?>
